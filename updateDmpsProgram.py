5326f750c838c2825e9a2e46546a45cc8fcb72b9
Dan Clegg <dan_clegg@byu.edu>
2015-12-09 13:58:43 -0700

    Removing version function references
    We can’t get the version reliably from \SIMPL\.~Program_Boot_Data at
    this point. Taking it out until we are at that point.

--- updateDmpsProgram.py
+++ updateDmpsProgram.py
@@ -1,42 +1,49 @@
 #!/usr/bin/env python
 #update.py
+import sys
 from datetime import datetime
 from elasticsearch import Elasticsearch,RequestsHttpConnection
-import sys
 import ftplib
 import os
+from parse import *
 import socket
 import telnetlib
 
 host = '10.6.36.51'
 port = 41795
 size = 1024
-output = None
-splusExists = None
-simplExists = None
-iptable = None
-info = None
-free = None
-ramfree = None
-ver = None
+output = ""
+splusExists = ""
+simplExists = ""
+iptable = ""
+info = ""
+free = ""
+ramfree = ""
+#ver = ""
 halt = 0
-haltReason = None
+haltReason = ""
 SIMPL = "SIMPL"
 SPLUS = "SPLUS"
 BAK = "USER"
 true = "true"
 false = "false"
+parserResult = ""
 telnetClient = telnetlib.Telnet()
 
 ### FUNCTIONS ###
 def openTelnet():
+	global telnetClient
 	telnetClient=telnetlib.Telnet(host)
 	telnetClient.read_until(b"DMPS-300-C>")  ## IT'S READY TO GO HERE
+	return telnetClient
 
 def closeTelnet():
+	global telnetClient
 	telnetClient.close()
+	return telnetClient
 
 def isTelnetLive():
+	global telnetClient
 	s=telnetClient.get_socket()
 	if s == 0:
 		return false
@@ -44,62 +51,67 @@ def isTelnetLive():
 		return true
 
 def testDir(dirToTest):
+	global telnetClient
 	telnetClient.write(b'isDir('+dirToTest+') \r')
 	output = (telnetClient.read_some())
 	telnetClient.read_until(b"DMPS-300-C>")
 	return output
 
 def iptable():
+	global telnetClient
 	telnetClient.write(b'iptable \r')
 	iptable = (telnetClient.read_some())
 	telnetClient.read_until(b"DMPS-300-C>")
 	return iptable
 
 def info():
+	global telnetClient
 	telnetClient.write(b'info \r')
 	info = (telnetClient.read_some())
 	telnetClient.read_until(b"DMPS-300-C>")
 	return info
 
 def free():
+	global telnetClient
 	telnetClient.write(b'free \r')
 	free = (telnetClient.read_some())
 	telnetClient.read_until(b"DMPS-300-C>")
 	return free
 
 def ramfree():
+	global telnetClient
 	telnetClient.write(b'ramfree \r')
 	ramfree = (telnetClient.read_some())
 	telnetClient.read_until(b"DMPS-300-C>")
 	return ramfree
 
-def version():
-	telnetClient.write(b'ver \r')
-	ver = (telnetClient.read_some())
-	telnetClient.read_until(b"DMPS-300-C>")
-	return ver
-
 def cd(dir):
+	global telnetClient
 	telnetClient.write(b'cd '+ dir +' \r')
 	output = (telnetClient.read_some())
 	output=telnetClient.read_until(b"DMPS-300-C>")
 	return output
 
 def copy(srcDir,dstDir,file):
+	global telnetClient
 	telnetClient.write(b'copyfile '+ srcDir +'\\'+ file +' '+ dstDir +'\\'+ file +' \r')
 	output = (telnetClient.read_some())
 	telnetClient.read_until(b"DMPS-300-C>")
 	return output
 
 def fn(func):
+	global telnetClient
 	telnetClient.write(b''+ func + ' \r')
 	output = telnetClient.read_until(b"DMPS-300-C>")
 	return output
 
-print 'At end of functions declaration \r'
 ### END FUNCTIONS ###
 
-openTelnet()
+#Change Directory to build dir
+projectDir=os.path.abspath(os.getcwd())
+os.chdir(projectDir + "/build")
+
+telnetClient=openTelnet()
 
 assert isTelnetLive() == true
 
@@ -131,12 +143,6 @@ if (halt <= 0):
 
 	#if testDir(SIMPL) == true:
 
-	#Change Directory to build dir
-	projectDir=os.path.abspath(os.getcwd())
-	os.chdir(projectDir + "/build")
-
-	cd 
-
 	## Move new program
 	#fn("stopprog")  # Stop the current DMPS program
 	#print output
@@ -144,11 +150,11 @@ if (halt <= 0):
 	#print output
 	#fn("progcom")
 	#print output
-	#fn("ver")
-	#print output
+	print free()
+
 else:
 	print "Error encountered: %d",haltReason
 
 
-closeTelnet()
+telnetClient=closeTelnet()
 assert isTelnetLive() == false
