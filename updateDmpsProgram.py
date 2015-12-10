30018a0b920e7e365d24df3dce41900799b4b452
Dan Clegg <dan_clegg@byu.edu>
2015-12-10 11:15:32 -0700

    Version is working

--- updateDmpsProgram.py
+++ updateDmpsProgram.py
@@ -12,6 +12,9 @@ import telnetlib
 host = '10.6.36.51'
 port = 41795
 size = 1024
+reqVer = ""
+upgradeVer = ""
+
 output = ""
 splusExists = ""
 simplExists = ""
@@ -19,7 +22,7 @@ iptable = ""
 info = ""
 free = ""
 ramfree = ""
-#ver = ""
+ver = ""
 halt = 0
 haltReason = ""
 SIMPL = "SIMPL"
@@ -30,10 +33,16 @@ false = "false"
 parserResult = ""
 telnetClient = telnetlib.Telnet()
 
+### HANDLE ARGUMENTS ###
+
+
+### END HANDLE ARGUMENTS ###
+
+
 ### FUNCTIONS ###
 def openTelnet():
 	global telnetClient
-	telnetClient=telnetlib.Telnet(host)
+	telnetClient=telnetlib.Telnet(host,port)
 	telnetClient.read_until(b"DMPS-300-C>")  ## IT'S READY TO GO HERE
 	return telnetClient
 
@@ -58,32 +67,55 @@ def testDir(dirToTest):
 	return output
 
 def iptable():
-	global telnetClient
+	global telnetClient,iptable
 	telnetClient.write(b'iptable \r')
 	iptable = (telnetClient.read_some())
 	telnetClient.read_until(b"DMPS-300-C>")
 	return iptable
 
 def info():
-	global telnetClient
+	global telnetClient,info
 	telnetClient.write(b'info \r')
 	info = (telnetClient.read_some())
 	telnetClient.read_until(b"DMPS-300-C>")
 	return info
 
 def free():
-	global telnetClient
+	global telnetClient,free
 	telnetClient.write(b'free \r')
 	free = (telnetClient.read_some())
 	telnetClient.read_until(b"DMPS-300-C>")
 	return free
 
 def ramfree():
-	global telnetClient
+	global telnetClient,ramfree
 	telnetClient.write(b'ramfree \r')
 	ramfree = (telnetClient.read_some())
 	telnetClient.read_until(b"DMPS-300-C>")
 	return ramfree
+
+def version():
+	global telnetClient,ver
+	telnetClient.write(b'\r\rprogcomments \r')
+	localVer = (telnetClient.read_until("Program File"))
+	split = localVer.split("TEC")
+	retVal = ""
+	for line in split:
+		if line.find("v") <> -1:
+			retVal = line
+
+	splitLine = retVal.split(" ")
+	for word in splitLine:
+		if word.find(".") <> -1:
+			word = word.strip()
+			word = word.replace("Program","")
+			word = word.replace("\r","")
+			word = word.replace("\n","")
+			retVal = word
+
+	telnetClient.read_until(b"DMPS-300-C>")
+	ver = retVal
+	return retVal
 
 def cd(dir):
 	global telnetClient
@@ -105,7 +137,27 @@ def fn(func):
 	output = telnetClient.read_until(b"DMPS-300-C>")
 	return output
 
+def push(file):
+	global telnetClient
+
+
 ### END FUNCTIONS ###
+
+### BACKUP ###
+
+#projectDir=os.path.abspath(os.getcwd())
+#os.chdir(projectDir + "/bak")
+#
+#
+#telnetClient=openTelnet()
+#assert isTelnetLive() == true
+#
+#
+#
+#telnetClient=closeTelnet()
+#assert isTelnetLive() == false
+
+### END BAK ###
 
 #Change Directory to build dir
 projectDir=os.path.abspath(os.getcwd())
@@ -122,39 +174,41 @@ assert isTelnetLive() == true
 #info()
 
 ## Version gate logic
-#version()
+version()
+print ver
+
 
 ## freespace gate logic
 #free()
 
 ## Move ahead
-if (halt <= 0):
-	print 'moving ahead \r'
-	
-	##BACKUP STEPS
-	#if testDir(BAK) == true:
-	#	print 'cd to BAK \r'
-	#	cd(BAK)
-	#	print 'copying ~.Manifest \r'
-	#	copy(SIMPL,BAK,"~.Manifest")
-	#	print 'copy done \r'
-	#else:
-	#	print "\r"
-
-	#if testDir(SIMPL) == true:
-
-	## Move new program
-	#fn("stopprog")  # Stop the current DMPS program
-	#print output
-	#fn("progreset")
-	#print output
-	#fn("progcom")
-	#print output
-	print free()
-
-else:
-	print "Error encountered: %d",haltReason
-
-
+#if (halt <= 0):
+#	print 'moving ahead \r'
+#	
+#	##BACKUP STEPS
+#	#if testDir(BAK) == true:
+#	#	print 'cd to BAK \r'
+#	#	cd(BAK)
+#	#	print 'copying ~.Manifest \r'
+#	#	copy(SIMPL,BAK,"~.Manifest")
+#	#	print 'copy done \r'
+#	#else:
+#	#	print "\r"
+#
+#	#if testDir(SIMPL) == true:
+#
+#	## Move new program
+#	#fn("stopprog")  # Stop the current DMPS program
+#	#print output
+#	#fn("progreset")
+#	#print output
+#	#fn("progcom")
+#	#print output
+#	print free()
+#
+#else:
+#	print "Error encountered: %d",haltReason
+#
+#
 telnetClient=closeTelnet()
 assert isTelnetLive() == false
